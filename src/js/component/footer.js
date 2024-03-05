fa97056e8cecfd3905da2509cbc8d9b5e497deac
jant7070 <josemorillo702@gmail.com>
2024-03-05 19:20:48 -0400

    Footer changes and home inline styles removed

--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -1,10 +1,39 @@
 import React, { Component } from "react";
+import logo from "../../img/logo.png"
+import { Link } from "react-router-dom";
+
 
 export const Footer = () => (
-	<footer className="footer mt-auto py-3 text-center">
-		<p>
-			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
-		</p>
+	<footer className="container-fluid bg-dark text-light footer my-6 mb-0 py-6 wow fadeIn" data-wow-delay="0.1s">
+		<div className="container">
+			<div className="row g-5">
+				<div className="col-lg-3 col-md-6 justify-content-center text-center">
+					<img className="footer-logo img-fluid" src={logo} />
+				</div>
+				<div className="col-lg-3 col-md-6 justify-content-center text-center">
+					<h4 className="contacto-footer mb-4">Contáctanos</h4>
+					<p className="mb-2"><i className="fa fa-map-marker-alt me-3"></i>Centro Comercial Vizcaya, piso 3.</p>
+					<p className="mb-2"><i className="fa fa-phone me-3"></i>+58 414 211 7731</p>
+					<p className="mb-2"><i className="fa fa-envelope me-3"></i>info@example.com</p>
+				</div>
+				<div className="col-lg-3 col-md-6 justify-content-center text-center">
+					<h4 className="mb-4 links-footer ">Popular Links</h4>
+					<div className="d-flex flex-column">
+						<Link className="btn btn-link" href="">Sobre nosotros</Link>
+						<Link className="btn btn-link" href="">Clases y servicios</Link>
+						<Link className="btn btn-link" to="/">Contacto</Link>
+					</div>
+				</div>
+				<div className="col-lg-3 col-md-6 justify-content-center text-center">
+					<h4 className="mb-4 follow-footer">Síguenos</h4>
+					<div className="d-flex pt-2 justify-content-center text-center">
+						<a className="btn btn-square btn-outline-light me-1" href=""><i className="fab fa-twitter"></i></a>
+						<a className="btn btn-square btn-outline-light me-1" href=""><i className="fab fa-facebook-f"></i></a>
+						<a className="btn btn-square btn-outline-light me-1" href=""><i className="fab fa-youtube"></i></a>
+						<a className="btn btn-square btn-outline-light me-0" href=""><i className="fab fa-instagram"></i></a>
+					</div>
+				</div>
+			</div>
+		</div>
 	</footer>
 );
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -37,7 +37,7 @@ export const Home = () => {
 								<div className="container">
 									<div className="row justify-content-center">
 										<div className="col-lg-7">
-											<img className="img-fluid position-relative mt-auto favicon" src={light} style={{ width: "250px" }} />
+											<img className="img-fluid position-relative mt-auto favicon" src={light} />
 											<h1 className="display-2 mb-5 animate__animated animate__slideInDown">Making The World a Brighter Place</h1>
 											<Link href="" className="btn btn-navBar py-sm-3 px-sm-5">Sobre nosotros</Link>
 											<Link href="" className="btn btn-navBar py-sm-3 px-sm-5 ms-3">Nuestras clases</Link>
@@ -78,9 +78,9 @@ export const Home = () => {
 				<div className="container py-5 pt-lg-0">
 					<div className="row gx-0">
 						<div className="col-lg-4 wow animate__animated animate__fadeIn" data-wow-delay="0.1s">
-							<div className="bg-white shadow d-flex align-items-center h-100 p-4" style={{ minHeight: "150px" }}>
+							<div className="bg-white facts-label shadow d-flex align-items-center h-100 p-4">
 								<div className="d-flex">
-									<div className="flex-shrink-0 facts-icons btn-lg-square rounded rounded-3 ">
+									<div className="flex-shrink-0 bg-success btn-lg-square rounded rounded-3 ">
 										<i className="fa fa-people-carry text-white"></i>
 									</div>
 									<div className="ps-4">
@@ -91,9 +91,9 @@ export const Home = () => {
 							</div>
 						</div>
 						<div className="col-lg-4 wow animate__animated animate__fadeIn" data-wow-delay="0.3s">
-							<div className="bg-white shadow d-flex align-items-center h-100 p-4" style={{ minHeight: "150px" }}>
+							<div className="bg-white facts-label shadow d-flex align-items-center h-100 p-4">
 								<div className="d-flex">
-									<div className="flex-shrink-0 rounded rounded-3 btn-lg-square facts-icons">
+									<div className="flex-shrink-0 bg-warning rounded rounded-3 btn-lg-square">
 										<i className="fa fa-users text-white"></i>
 									</div>
 									<div className="ps-4">
@@ -104,9 +104,9 @@ export const Home = () => {
 							</div>
 						</div>
 						<div className="col-lg-4 wow animate__animated animate__fadeIn" data-wow-delay="0.5s">
-							<div className="bg-white shadow d-flex align-items-center h-100 p-4" style={{ minHeight: "150px" }}>
+							<div className="bg-white facts-label shadow d-flex align-items-center h-100 p-4">
 								<div className="d-flex">
-									<div className="flex-shrink-0 rounded rounded-3 btn-lg-square facts-icons">
+									<div className="flex-shrink-0 bg-danger rounded rounded-3 btn-lg-square">
 										<i className="fa fa-book-open text-white"></i>
 									</div>
 									<div className="ps-4">
@@ -124,15 +124,15 @@ export const Home = () => {
 				<div className="container">
 					<div className="row g-5">
 						<div className="col-lg-6 wow animate__animated animate__fadeInUp" data-wow-delay="0.1s">
-							<div className="position-relative overflow-hidden ps-5 pt-5 h-100" style={{ minHeight: "400px" }}>
-								<img className="position-absolute w-100 h-100" src={teacher} alt="" style={{ objectFit: "cover" }} />
-								<img className="position-absolute top-0 start-0 bg-white pe-3 pb-3" src={teacher} alt="" style={{ width: "200px", height: "200px", objectFit: "cover" }} />
+							<div className="position-relative overflow-hidden ps-5 pt-5 h-100 about-container">
+								<img className="position-absolute w-100 h-100 img-1-about" src={teacher} />
+								<img className="position-absolute top-0 start-0 img-2-about bg-white pe-3 pb-3" src={teacher} />
 							</div>
 						</div>
 						<div className="col-lg-6 wow fadeInUp" data-wow-delay="0.5s">
 							<div className="h-100">
-								<h6 className="text-uppercase mb-2">Sobre nosotros</h6>
-								<h1 className="display-6 mb-4">Te ayudamos a encontrar tu <strong style={{ color: "#febd5b" }}>BRIGHT SIDE</strong></h1>
+								<h4 className="text-uppercase mb-2">Sobre nosotros</h4>
+								<h1 className="display-6 mb-4">Te ayudamos a encontrar tu <strong>BRIGHT SIDE</strong></h1>
 								<p>Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit. Aliqu diam amet diam et eos. Clita erat ipsum et lorem et sit, sed stet lorem sit clita duo justo magna dolore erat amet</p>
 								<p className="mb-4">Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit. Aliqu diam amet diam et eos. Clita erat ipsum et lorem et sit, sed stet lorem sit clita duo justo magna dolore erat amet</p>
 								<div className="row g-2 mb-4 pb-2">
@@ -168,10 +168,10 @@ export const Home = () => {
 							<h1 className="display-6 mb-4">Clases y servicios</h1>
 						</div>
 						<div className="row g-4 justify-content-center">
-							<div className="col-lg-4 col-md-6 wow fadeInUp py-3" style={{ background: "#FD8A8A" }} data-wow-delay="0.1s">
+							<div className="col-lg-4 col-md-6 wow fadeInUp py-3 art-class" data-wow-delay="0.1s">
 								<div className="courses-item d-flex flex-column bg-white overflow-hidden h-100">
 									<div className="text-center p-4 pt-0">
-										<h2 className="mb-3" style={{ color: "#FD8A8A" }}>Art class</h2>
+										<h2 className="mb-3 class-title">Art class</h2>
 										<p>
 											Nuestras clases de arte para niños de 5 años en adelante
 											son un espacio de autoconocimiento, expresión libre y aprendizaje,
@@ -186,10 +186,10 @@ export const Home = () => {
 									</div>
 								</div>
 							</div>
-							<div className="col-lg-4 col-md-6 wow fadeInUp py-3" style={{ background: "#A8D1D1" }} data-wow-delay="0.3s">
+							<div className="col-lg-4 col-md-6 wow fadeInUp py-3 english-class" data-wow-delay="0.3s">
 								<div className="courses-item d-flex flex-column bg-white overflow-hidden h-100">
 									<div className="text-center p-4 pt-0">
-										<h2 className="mb-3" style={{ color: "#A8D1D1" }}>English class</h2>
+										<h2 className="mb-3 class-title">English class</h2>
 										<p>
 											Clases grupales completamente en Inglés, donde trabajamos el
 											desarrollo del niño a través de la exploración
@@ -204,10 +204,10 @@ export const Home = () => {
 									</div>
 								</div>
 							</div>
-							<div className="col-lg-4 col-md-6 wow fadeInUp py-3" style={{ background: "#febd5b" }} data-wow-delay="0.5s">
+							<div className="col-lg-4 col-md-6 wow fadeInUp py-3 day-care" data-wow-delay="0.5s">
 								<div className="courses-item d-flex flex-column bg-white overflow-hidden h-100">
 									<div className="text-center p-4 pt-0">
-										<h2 className="mb-3" style={{ color: "#febd5b" }}>Day care</h2>
+										<h2 className="mb-3 class-title">Day care</h2>
 										<p>
 											Day care bilingüe (Inglés-Español) para niños entre 1 y 3 años.
 											Acompañamos a nuestros alumnos en su desarrollo libre, integral, intelectual,
@@ -224,7 +224,7 @@ export const Home = () => {
 							</div>
 							<div className="col-lg-8 my-6 mb-0 wow fadeInUp" data-wow-delay="0.1s">
 								<div className="bg-white shadow rounded rounded-3 text-center p-5">
-									<h1 className="mb-4" style={{ color: "#FD8A8A" }}>Contáctanos</h1>
+									<h1 className="mb-4">Contáctanos</h1>
 									<form>
 										<div className="row g-3">
 											<div className="col-sm-6">
@@ -280,13 +280,13 @@ export const Home = () => {
 										<img className="img-fluid" src={rigoImage} alt="" />
 										<div className="team-social text-center">
 											<a className="btn btn-square  border-2 m-1" href=""><i className="fab fa-facebook-f text-warning"></i></a>
-											<a className="btn btn-square  border-2 m-1" href=""><i className="fab fa-twitter"></i></a>
-											<a className="btn btn-square  border-2 m-1" href=""><i className="fab fa-instagram"></i></a>
+											<a className="btn btn-square  border-2 m-1" href=""><i className="fab fa-twitter text-danger"></i></a>
+											<a className="btn btn-square  border-2 m-1" href=""><i className="fab fa-instagram text-success"></i></a>
 										</div>
 									</div>
 									<div className="bg-light text-center p-4">
-										<h5 className="mt-2">Full Name</h5>
-										<span>Trainer</span>
+										<h5 className="mt-2">Maria Hernandez</h5>
+										<span>Teacher</span>
 									</div>
 								</div>
 							</div>
@@ -301,8 +301,8 @@ export const Home = () => {
 										</div>
 									</div>
 									<div className="bg-light text-center p-4">
-										<h5 className="mt-2">Full Name</h5>
-										<span>Trainer</span>
+										<h5 className="mt-2">Mariana Hernandez</h5>
+										<span>Teacher</span>
 									</div>
 								</div>
 							</div>
@@ -335,57 +335,6 @@ export const Home = () => {
 									<div className="bg-light text-center p-4">
 										<h5 className="mt-2">Full Name</h5>
 										<span>Trainer</span>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-
-				<div className="container-xxl py-6">
-					<div className="container">
-						<div className="text-center mx-auto mb-5 wow fadeInUp" data-wow-delay="0.1s" style={{ maxWidth: "500px" }}>
-							<h6 className="text-primary text-uppercase mb-2">Testimonial</h6>
-							<h1 className="display-6 mb-4">What Our Clients Say!</h1>
-						</div>
-						<div className="row justify-content-center">
-							<div className="col-lg-8 wow fadeInUp" data-wow-delay="0.1s">
-								<div className="owl-carousel testimonial-carousel">
-									<div className="testimonial-item text-center">
-										<div className="position-relative mb-5">
-											<img className="img-fluid rounded-circle mx-auto" src={rigoImage} alt="" />
-											<div className="position-absolute top-100 start-50 translate-middle d-flex align-items-center justify-content-center bg-white rounded-circle" style={{ width: "60px", height: "60px" }}>
-												<i className="fa fa-quote-left fa-2x text-primary"></i>
-											</div>
-										</div>
-										<p className="fs-4">Dolores sed duo clita tempor justo dolor et stet lorem kasd labore dolore lorem ipsum. At lorem lorem magna ut et, nonumy et labore et tempor diam tempor erat.</p>
-										<hr className="w-25 mx-auto" />
-										<h5>Client Name</h5>
-										<span>Profession</span>
-									</div>
-									<div className="testimonial-item text-center">
-										<div className="position-relative mb-5">
-											<img className="img-fluid rounded-circle mx-auto" src={rigoImage} alt="" />
-											<div className="position-absolute top-100 start-50 translate-middle d-flex align-items-center justify-content-center bg-white rounded-circle" style={{ width: "60px", height: "60px" }}>
-												<i className="fa fa-quote-left fa-2x text-primary"></i>
-											</div>
-										</div>
-										<p className="fs-4">Dolores sed duo clita tempor justo dolor et stet lorem kasd labore dolore lorem ipsum. At lorem lorem magna ut et, nonumy et labore et tempor diam tempor erat.</p>
-										<hr className="w-25 mx-auto" />
-										<h5>Client Name</h5>
-										<span>Profession</span>
-									</div>
-									<div className="testimonial-item text-center">
-										<div className="position-relative mb-5">
-											<img className="img-fluid rounded-circle mx-auto" src={rigoImage} alt="" />
-											<div className="position-absolute top-100 start-50 translate-middle d-flex align-items-center justify-content-center bg-white rounded-circle" style={{ width: "60px", height: "60px" }}>
-												<i className="fa fa-quote-left fa-2x text-primary"></i>
-											</div>
-										</div>
-										<p className="fs-4">Dolores sed duo clita tempor justo dolor et stet lorem kasd labore dolore lorem ipsum. At lorem lorem magna ut et, nonumy et labore et tempor diam tempor erat.</p>
-										<hr className="w-25 mx-auto" />
-										<h5>Client Name</h5>
-										<span>Profession</span>
 									</div>
 								</div>
 							</div>
